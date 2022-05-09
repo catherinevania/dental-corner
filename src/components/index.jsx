784de98d8075bcdc22c6be2e4a98d2e4882070d6
daffayanuardhana <daffayanuardhana@gmail.com>
2022-05-09 15:56:48 +0700

    Daffa [ADD] custom div

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -3,5 +3,6 @@ import { colors } from "./Colors";
 import { Input } from "./Input";
 import { Card } from "./Card";
 import { Header, Title, Paragraph, Label } from "./Typography";
+import { Flexer } from "./CustomDiv";
 
-export { Button, Input, colors, Card, Header, Title, Paragraph, Label };
+export { Button, Input, colors, Card, Header, Title, Paragraph, Label, Flexer };
