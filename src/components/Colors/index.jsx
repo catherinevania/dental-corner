91c8c9f75d0a8ebc81e5ca78b1c5465c47c70835
catherinevania <catherinevania@student.ub.ac.id>
2022-05-09 15:35:01 +0700

    ketrin [MODIFY] colors and button

--- src/components/Colors/index.jsx
+++ src/components/Colors/index.jsx
@@ -3,7 +3,7 @@ export const colors = {
   primaryLight: "#FDE7DD",
   primaryDark: "#EC6324",
   gray: "#4B4B4B",
-  grayLight: "#A8A8A8",
+  grayLight: "#CBCCCC",
   grayDark: "#191919",
   white: "#FDFDFC",
   black: "#0A0C0B",
